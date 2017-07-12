51a1dfe69a446591a04602e570217ef52f70cc2e
Hongchul Kim <gsjune@naver.com>
2017-07-13 04:02:15 +0900

    Main 내용 변경, hello 메소드 추가

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,12 @@
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello world!!!");
+
+        System.out.println("안녕하세요");
+    }
+
+    public static void hello() {
+
     }
 }
